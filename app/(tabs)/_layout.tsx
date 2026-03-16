4bd69d4303d930dab371381a30af853b59ab948e
AdrianeSS <adriane_silvasanches@hotmail.com>
2026-03-15 21:14:57 -0300

    feat: atualizando nome da tela

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,7 +21,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Dashboard',
+          title: 'Home',
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
           ),
